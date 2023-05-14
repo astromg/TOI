0a7544653d6f93035348f49203df7ab0ed68270d
Marek Górski <mareksgorski@gmail.com>
2023-05-14 23:31:59 +0200

    investigating a bug

--- plan_gui.py
+++ plan_gui.py
@@ -264,7 +264,7 @@ class PlanGui(QWidget, BaseAsyncWidget, metaclass=MetaAsyncWidgetQtWidget):
           print(os.environ)
 
           self.File_dialog = QFileDialog
-          self.File_dialog.DontUseNativeDialog
+          #self.File_dialog.DontUseNativeDialog
           self.fileName = self.File_dialog.getOpenFileName(None,"Open file",".")[0]
           #self.fileName=str(QFileDialog.getOpenFileName(None,"Open file",".")[0])
           print(" =============================== ",self.fileName)
