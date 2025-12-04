41e95da921e3953f0e37216c33f05eff033fc4d0
Marek Gorski <mgorski@camk.edu.pl>
2025-12-04 15:53:26 +0100

    tpg v2.0 integration

--- plan_gui.py
+++ plan_gui.py
@@ -30,7 +30,7 @@ from base_async_widget import MetaAsyncWidgetQtWidget, BaseAsyncWidget
 from pyaraucaria.coordinates import *
 
 from toi_lib import *
-from telescope_plan_generator import telescope_plan_generator as tpg
+from tpg.telescope_plan_generator import TelescopePlanGenerator as tpg
 from ctc import CycleTimeCalc
 
 
