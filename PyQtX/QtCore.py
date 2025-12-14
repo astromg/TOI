10a4a52cfafc3688e688f0c9135c80264d565a81
Mikołaj Kałuszyński <mkalusz@camk.edu.pl>
2025-12-14 19:13:40 -0300

    Add color compatibility aliases in QtCore
    Added compatibility aliases for various colors in Qt.

--- PyQtX/QtCore.py
+++ PyQtX/QtCore.py
@@ -21,5 +21,23 @@ try:
     # Compatibility aliases for case sensitivity
     Qt.CaseInsensitive = Qt.CaseSensitivity.CaseInsensitive
     Qt.CaseSensitive = Qt.CaseSensitivity.CaseSensitive
+    # Compatibility aliases for colors (GlobalColor)
+    Qt.black = Qt.GlobalColor.black
+    Qt.white = Qt.GlobalColor.white
+    Qt.gray = Qt.GlobalColor.gray
+    Qt.red = Qt.GlobalColor.red
+    Qt.blue = Qt.GlobalColor.blue
+    Qt.green = Qt.GlobalColor.green
+    Qt.cyan = Qt.GlobalColor.cyan
+    Qt.magenta = Qt.GlobalColor.magenta
+    Qt.yellow = Qt.GlobalColor.yellow
+    Qt.darkRed = Qt.GlobalColor.darkRed
+    Qt.darkGreen = Qt.GlobalColor.darkGreen
+    Qt.darkBlue = Qt.GlobalColor.darkBlue
+    Qt.darkCyan = Qt.GlobalColor.darkCyan
+    Qt.darkMagenta = Qt.GlobalColor.darkMagenta
+    Qt.darkYellow = Qt.GlobalColor.darkYellow
+    Qt.darkGray = Qt.GlobalColor.darkGray
+    Qt.lightGray = Qt.GlobalColor.lightGray
 except ImportError:
     from PyQt5.QtCore import *
