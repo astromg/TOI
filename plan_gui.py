f117d3142ac4238594ca6228e68133e9cc30b86d
observer <ocacontrolroom@gmail.com>
2026-04-19 21:33:07 -0400

    developement

--- plan_gui.py
+++ plan_gui.py
@@ -1152,55 +1152,21 @@ class TPGWindow(BaseWindow):
             obs.date = dt
 
             sun = ephem.Sun()
-            try:
-                sunset = obs.previous_setting(sun)
-                sunset_dt = ephem.Date(sunset).datetime()
 
-                delta = dt - sunset_dt
+            sunset = obs.previous_setting(sun)
+            sunset = ephem.Date(sunset).datetime()
 
-                within_2h = datetime.timedelta(0) <= delta <= datetime.timedelta(hours=2)
-                date_changed = dt.date() != sunset_dt.date()
+            print(sunset, dt)
 
-                # Adjust date only if we are within 2 hours of sunset and have passed midnight
-                if within_2h and date_changed:
-                    dt = dt - datetime.timedelta(days=1)
-
-                self.ut_e.setText(dt.strftime("%Y-%m-%d"))
-            except ephem.NeverUpError:
-                # Handle the case where the sun never sets (e.g., polar day)
-                self.ut_e.setText(dt.strftime("%Y-%m-%d"))
+            if dt - sunset < datetime.timedelta(hours=2):
+                if sunset.date() != dt.date():
+                    time2set = dt - datetime.timedelta(days=1)
+                else:
+                    time2set = dt
+                self.ut_e.setText(time2set.strftime("%Y-%m-%d"))
         else:
             self.ut_e.setText(dt.strftime("%Y-%m-%d %H:%M:%S"))
 
-    # def sunset_changed(self):
-    #
-    #     dt = datetime.datetime.strptime(self.parent.parent.ut,"%Y/%m/%d %H:%M:%S")
-    #
-    #     if self.sunset_c.isChecked():
-    #         obs = ephem.Observer()
-    #         obs.lat = self.parent.parent.observatory[0]
-    #         obs.lon = self.parent.parent.observatory[1]
-    #         obs.elevation = float(self.parent.parent.observatory[2])
-    #         obs.date = dt
-    #
-    #         sun = ephem.Sun()
-    #         try:
-    #             sunset = obs.previous_setting(sun)
-    #             sunset_dt = ephem.Date(sunset).datetime()
-    #
-    #             delta = dt - sunset_dt
-    #
-    #             within_2h = datetime.timedelta(0) <= delta <= datetime.timedelta(hours=2)
-    #             date_changed = dt.date() != sunset_dt.date()
-    #
-    #             if within_2h and date_changed:
-    #                 dt = dt - datetime.timedelta(days=1)
-    #
-    #             self.ut_e.setText(dt.strftime("%Y-%m-%d"))
-    #         except ephem.NeverUpError:
-    #             self.ut_e.setText(dt.strftime("%Y-%m-%d"))
-    #     else:
-    #         self.ut_e.setText(dt.strftime("%Y-%m-%d %H:%M:%S"))
 
     def _smart_sunset_checkbox(self):
         dt = datetime.datetime.strptime(self.parent.parent.ut, "%Y/%m/%d %H:%M:%S")
