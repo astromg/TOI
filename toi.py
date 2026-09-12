293e9ad78acb0cf3709e931c33ee432cb92db7f0
MMiirrkk <cot@wp.pl>
2026-09-12 14:56:29 +0200

    ctc 6d

--- toi.py
+++ toi.py
@@ -2242,7 +2242,7 @@ class TOI(QtWidgets.QWidget, BaseAsyncWidget, metaclass=MetaAsyncWidgetQtWidget)
                             if "seq" in self.plan[tel][i]["ob"] and self.tel_acces[tel] and \
                                     self.plan[tel][i]["meta"].get("slotTime_rm") != rm_now:
                                 calc_slotTime = True
-
+                        print(self.tel_acces[tel])
                         if calc_slotTime:
                             if "sec" in self.plan[tel][i]["ob"].keys():
                                 # slotTime = float(self.plan[tel][i]["ob"]["sec"])
