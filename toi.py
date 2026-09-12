0e56120e66b9341cc21ac239cd3c5d7dbf3fdee3
MMiirrkk <cot@wp.pl>
2026-09-12 15:53:39 +0200

    ctc 6j

--- toi.py
+++ toi.py
@@ -2293,8 +2293,8 @@ class TOI(QtWidgets.QWidget, BaseAsyncWidget, metaclass=MetaAsyncWidgetQtWidget)
                                 #
                                 # self.plan[tel][i]["meta"]["slotTime"] = slotTime * 24 * 3600
                                 print(f'xxxxxxxxxxxx{self.plan[tel][i]["block"]}xxxxxxxxxxxxxxx')
-                                print(self.ctc_dat[tel]._start_time)
-                                print(self.ctc_dat[tel]._time_length_list)
+                                print(self.ctc_dat[tel]._start_time + datetime.timedelta(seconds=self._time_length))
+                                # print(self.ctc_dat[tel]._time_length_list)
                                 slotTime, _ = self._calc_ctc(
                                     tel=tel,
                                     rm_now=rm_now,
